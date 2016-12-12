501acee8c2e55068a47186787ac88437dbf8e67b
Kevin Pei <kevin@v1020-wn-201-70.campus-dynamic.uwaterloo.ca>
2016-12-12 15:55:26 -0500

    Auto port

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2365'
+            port: process.env.PORT
         }
     },
 

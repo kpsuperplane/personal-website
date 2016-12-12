d4df478b8be64b379d93b8e554c32cf3dae21e00
Kevin Pei <kevin@v1020-wn-201-70.campus-dynamic.uwaterloo.ca>
2016-12-12 16:18:52 -0500

    listen to 0

--- config.js
+++ config.js
@@ -65,7 +65,7 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: process.env.PORT
         }

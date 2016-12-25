12c27c90ff731bf650c2ff89ecda7c2321f03b4d
Kevin Pei <kevin-pei@hotmail.com>
2016-12-25 14:30:20 -0500

    Use connection string

--- config.js
+++ config.js
@@ -15,14 +15,14 @@ config = {
         mail: {},
         database: {
             client: 'pg',
-            connection: {
+            connection: process.env.DATABASE_URL/*{
                 host: process.env.DB_HOST,
                 user: process.env.DB_USER,
                 password: process.env.DB_PASS,
                 database: process.env.DB_NAME,
                 port: '5432',
                 ssl: true
-            },
+            }*/,
             debug: false
         },
 

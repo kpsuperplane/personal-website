37f159830b9ee364d7bac3a8628dc607915ae843
Kevin Pei <kevin-pei@hotmail.com>
2017-01-03 19:40:25 -0500

    remove border radius from images and add page controller

--- content/themes/personal/source/index.js
+++ content/themes/personal/source/index.js
@@ -1,6 +1,7 @@
 import './style/style.scss';
 import {u} from 'umbrellajs';
 import home from './pages/home';
+import page from './pages/page';
 import serviceManager from './services/serviceManager';
 import onloadService from './services/onloadService';
 import navigationService from './services/navigationService';
@@ -16,7 +17,7 @@ onloadServiceInstance.addListener(function(){
 });
 
 if(u('body').hasClass('home-template')) home.init(); //initialize home js on homepage 
-
+if(u('body').hasClass('page-template')) page.init(); //initialize page js on pages
 
 u('iframe').wrap('<div class="video-wrapper">'); //pretty videos
 

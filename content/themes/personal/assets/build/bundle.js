19fb85381dc110aaf9d7bce071a3bd83d479295e
Kevin Pei <kevin-pei@hotmail.com>
2017-12-18 22:08:21 -0500

    Styling fixes

--- content/themes/personal/assets/build/bundle.js
+++ content/themes/personal/assets/build/bundle.js
@@ -86,7 +86,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "-" + "005f865789a411b20591" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "-" + "17bc35f23aff1aa87393" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {

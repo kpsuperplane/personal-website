23333997df216cd18eb70f9bcbd4e5512c0cbfca
Kevin Pei <kevin-pei@hotmail.com>
2017-12-28 00:06:33 -0500

    Try absolute positioning for performance

--- content/themes/personal/assets/build/bundle.js
+++ content/themes/personal/assets/build/bundle.js
@@ -86,7 +86,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "-" + "f118066ba651c52610af" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "-" + "f39e70e7f54caf859163" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {

47023e3e4a15878b4a812c855c8d901bbacf3b38
Kevin Pei <kevin-pei@hotmail.com>
2018-02-03 23:09:36 -0500

    Mid-range-quality

--- content/themes/personal/src/views/Home.tsx
+++ content/themes/personal/src/views/Home.tsx
@@ -508,11 +508,12 @@ export default class Home extends View<{opened: boolean, posts: PostInterface[] 
         const { posts, projects, mouseMode, opened } = this.state!!;
         const { type, downlink } = ((navigator as any).connection || {type: undefined, downlink: undefined});
         const fastConnection = (downlink === undefined || downlink >= 3.5);
+        const reallyFastConnection = (downlink >= 7);
         const assumeWifi = type ? ((type === 'wifi' || type === 'ethernet'  || type === 'mixed') && fastConnection) : (downlink ? (!this.isMobile && fastConnection) : !this.isMobile);
         return (<div className={'home-component' + (mouseMode ? ' mousemode' : '')} ref={this.attachWrapper}>
             <div className={'home-video-wrapper' + (opened ? ' opened' : '') }>
                 <video loop="loop" autoplay="autoplay" data-object-fit="cover" muted="muted" className="home-video" playsinline ref={this.attachVideo}>
-                    <source src={'/assets/home-video-' + (assumeWifi ? 'desktop' : 'mobile') + '.mp4'} type="video/mp4" />
+                    <source src={'/assets/home-video' + (assumeWifi ? (reallyFastConnection ? '-desktop' : '') : '-mobile') + '.mp4'} type="video/mp4" />
                 </video>
             </div>
             <div className={'home-content' + (opened ? ' opened' : '') } ref={this.attachHero}>

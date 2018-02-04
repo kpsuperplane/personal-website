47e4c3e8997246d96d949810c802cccf3ccd2200
Kevin Pei <kevin-pei@hotmail.com>
2018-02-04 10:52:26 -0500

    Prepare for public deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo "LogLevel=quiet" > ~/.ssh/config
-git remote add dokku dokku@$server_ip:beta
+git remote add dokku dokku@$server_ip:2017
 echo -e "Host ${server_ip}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git push -f dokku 2017:master
+git push -f dokku master:master

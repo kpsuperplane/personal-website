d31869b422458b59c50dcaeb08fa88e5378857fa
Kevin Pei <kevin-pei@hotmail.com>
2018-02-04 11:42:03 -0500

    don't use htpps

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dbString = require('url').parse(process.env.DATABASE_URL),
       auth = dbString.auth.split(':');
 console.log(JSON.stringify({
-  "url": "https://kevinpei.com/",
+  "url": "kevinpei.com",
   "server": {
     host: '0.0.0.0',
     port: 5000

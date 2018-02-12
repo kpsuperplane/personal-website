c14081ff683be9328b6341c81c12e9a47cb4c9b6
Kevin Pei <kpei98@gmail.com>
2018-02-12 16:36:10 -0500

    Revert "Polyfill remove"
    This reverts commit 1844a811dbf4494a9637f8ba483471abb3b25cd3.

--- content/themes/personal/src/index.tsx
+++ content/themes/personal/src/index.tsx
@@ -1,6 +1,5 @@
 import 'es6-object-assign/auto';
 import 'es6-promise/auto';
-require('childnode-remove').polyfill();
 import './types';
 
 import createBrowserHistory from 'history/createBrowserHistory';
